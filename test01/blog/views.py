04e429731efae85a897e69e1e60e65f5b6beda81
aca <qym@jiagouyun.com>
2015-08-09 22:25:30 +0800

    Changes to be committed: 	new file:   blog/templates/index.html 	modified:   blog/views.py 	modified:   blog/views.pyc 	modified:   test01/urls.py 	modified:   test01/urls.pyc

--- test01/blog/views.py
+++ test01/blog/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader,Context
+from django.shortcuts import render_to_response
 # Create your views here.
+'''
 def index(req):
-	return HttpResponse('<h1>hello baby!</h1>')
+	t = loader.get_template('index.html')
+	c = Context({})
+	return HttpResponse( t.render(c))	
+'''
+
+class Person():
+#class Person(object):
+	def __init__(self,name,age,sex):
+		self.name = name
+		self.sex = sex
+		self.age = age
+	def say(self):
+		return "I'm " + self.name
+
+def index(req):
+#	user = {'name':'aca','age':29,'sex':'male'}
+	user = Person('aca',30,'male')
+	book_list = ['python','php','java','c']
+	return render_to_response('index.html',{'title':'fuck', 'user':user,'book_list':book_list})
+#	return HttpResponse('<h1>hello baby!</h1>')
--- test01/test01/urls.py
+++ test01/test01/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+#from blog.views import index
 
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^blog/$','blog.views.index'),
+#	url(r'^blog/$',index),
 ]
